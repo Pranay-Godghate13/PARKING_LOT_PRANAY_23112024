f8cc002959cfcdc6b3edec4d1fa133db72fe9a8e
Pranay <pranaygodghate13@gmail.com>
2024-11-23 22:48:12 +0530

    Add existing project files prior to the push to GitHub.

---
+++ src/main/java/com/parkinglotlld/parkinglotlld/ParkinglotlldApplication.java
@@ -0,0 +1,13 @@
+package com.parkinglotlld.parkinglotlld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotlldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotlldApplication.class, args);
+	}
+
+}
